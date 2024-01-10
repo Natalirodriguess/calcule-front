0d754eb69bef10b3af137533eaa9873546f7dbc4
Natalí Rodrigues <natalyrodrigues43210@gmail.com>
2024-01-10 11:51:04 -0300

    [main] Criando design calculadora

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,51 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import {Component, NgModule} from '@angular/core';
+import {CalculeService} from "./service/calcule.service";
+import {HttpClientModule} from "@angular/common/http";
+import {CommonModule} from "@angular/common";
+import {RouterOutlet} from "@angular/router";
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [HttpClientModule, CommonModule, RouterOutlet],
+  standalone: true,
+  providers: [CalculeService]
 })
 export class AppComponent {
   title = 'calcule';
+
+  operadorUm: number;
+  operadorDois: number;
+  operador: string;
+  passo: number = 1;
+
+  constructor(protected calculeService: CalculeService) {
+  }
+
+  realizarPasso(){
+    this.passo = this.passo + 1;
+    if(this.passo >= 4){
+      this.passo = 1;
+    }
+  }
+
+  desfazerPasso(){
+    if(this.passo > 1){
+      this.passo = this.passo - 1;
+    }
+
+  }
+
+  buttonNumberClick(value: number) {
+      if (!this.operador) {
+        this.operadorUm = value;
+      } else {
+        this.operadorDois = value;
+      }
+  }
+  buttonOperadorClick(value: string) {
+    this.operador = value;
+  }
+
 }
--- src/app/dto/operacao.ts
+++ src/app/dto/operacao.ts
@@ -1,4 +1,11 @@
 export class Operacao {
+
   operadorUm: number;
   operadorDois: number;
+
+  constructor(operadorUm: number, operadorDois: number) {
+    this.operadorUm = operadorUm;
+    this.operadorDois = operadorDois;
+  }
+
 }
