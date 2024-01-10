0a4afa6cf7a873e5db372b8bed648eba596a8aee
Natalí Rodrigues <natalyrodrigues43210@gmail.com>
2024-01-10 12:53:33 -0300

    [testes] Reiniciando front calculadora

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import {Component, NgModule} from '@angular/core';
-import {CalculeService} from "./service/calcule.service";
 import {HttpClientModule} from "@angular/common/http";
 import {CommonModule} from "@angular/common";
 import {RouterOutlet} from "@angular/router";
@@ -10,42 +9,12 @@ import {RouterOutlet} from "@angular/router";
   styleUrl: './app.component.css',
   imports: [HttpClientModule, CommonModule, RouterOutlet],
   standalone: true,
-  providers: [CalculeService]
+
 })
 export class AppComponent {
   title = 'calcule';
-
-  operadorUm: number;
-  operadorDois: number;
-  operador: string;
-  passo: number = 1;
-
-  constructor(protected calculeService: CalculeService) {
+  constructor() {
   }
 
-  realizarPasso(){
-    this.passo = this.passo + 1;
-    if(this.passo >= 4){
-      this.passo = 1;
-    }
-  }
-
-  desfazerPasso(){
-    if(this.passo > 1){
-      this.passo = this.passo - 1;
-    }
-
-  }
-
-  buttonNumberClick(value: number) {
-      if (!this.operador) {
-        this.operadorUm = value;
-      } else {
-        this.operadorDois = value;
-      }
-  }
-  buttonOperadorClick(value: string) {
-    this.operador = value;
-  }
 
 }
